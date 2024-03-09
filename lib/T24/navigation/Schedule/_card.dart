878a360ec2647c32765c182760694acc8e95dcc4
Tejaswa2611 <118481392+Tejaswa2611@users.noreply.github.com>
2024-03-09 16:35:46 +0530

    schedule

--- lib/T24/navigation/Schedule/_card.dart
+++ lib/T24/navigation/Schedule/_card.dart
@@ -1,4 +1,43 @@
+// import 'package:flutter/material.dart';
+// import 'package:techkriti/T24/navigation/Schedule/datamodel.dart';
+
+// class CardWidget extends StatelessWidget {
+//   final CardData cardData;
+
+//   const CardWidget({Key? key, required this.cardData}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Card(
+//       color: Colors.transparent,
+//       elevation: 0,
+//       margin: const EdgeInsets.all(5.0),
+//       shape: RoundedRectangleBorder(
+//         borderRadius: BorderRadius.circular(18.0),
+//         side: const BorderSide(
+//           color: Colors.white,
+//           width: 1.5,
+//         ),
+//       ),
+//       child: ListTile(
+//         title: Text(
+//           cardData.event,
+//           style: const TextStyle(
+//             color: Colors.white,
+//           ),
+//         ),
+//         subtitle: Text(
+//           cardData.venue,
+//           style: const TextStyle(
+//             color: Colors.white,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
+import 'package:techkriti/T24/constants/colors_and_fonts.dart';
 import 'package:techkriti/T24/navigation/Schedule/datamodel.dart';
 
 class CardWidget extends StatelessWidget {
@@ -9,28 +48,58 @@ class CardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Colors.transparent, 
+      color: Colors.transparent,
       elevation: 0,
-      margin: const EdgeInsets.all(5.0), 
+      margin: const EdgeInsets.all(5.0),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(18.0),
         side: const BorderSide(
           color: Colors.white,
-          width: 1.5, 
+          width: 1.5,
         ),
       ),
-      child: ListTile(
-        title: Text(
-          cardData.title,
-          style: const TextStyle(
-            color: Colors.white, 
-          ),
-        ),
-        subtitle: Text(
-          cardData.content,
-          style: const TextStyle(
-            color: Colors.white, 
-          ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Left column with event and venue
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    cardData.event,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontFamily: headingFont,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.33),
+                  ),
+                  const SizedBox(height: 3.0),
+                  Text(
+                    cardData.venue,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontFamily: "Montserrat",
+                        letterSpacing: 1),
+                  ),
+                ],
+              ),
+            ),
+            // Right column with time
+            Text(
+              cardData.time,
+              style: const TextStyle(
+                color: Colors.white,
+                fontFamily: "Montserrat",
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/T24/navigation/Schedule/datamodel.dart
+++ lib/T24/navigation/Schedule/datamodel.dart
@@ -1,16 +1,19 @@
 class CardData {
-  final String title;
-  final String content;
+  final String event;
+  final String venue;
+  final String time;
 
   const CardData({
-    required this.title,
-    required this.content,
+    required this.event,
+    required this.venue,
+    required this.time,
   });
 
   factory CardData.fromMap(Map<String, dynamic> map) {
     return CardData(
-      title: map['title'] as String,
-      content: map['content'] as String,
+      event: map['event'] as String,
+      venue: map['venue'] as String,
+      time: map['time'] as String,
     );
   }
 }
